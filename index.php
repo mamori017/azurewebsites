dfc7ea9444963691d721b25f5268a4fa4f3cdb65
mamori017 <mamori017@outlook.jp>
2017-07-09 03:26:49 +0900

    For now

---
+++ index.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta content="mamori017" name="Keywords" />
+	<meta content="mamori017" name="Description" />
+	<meta content="mamori017" name="Author" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>mamori017.azurewebsites.net</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
+	<link href="css/style.css" rel="stylesheet"/>
+</head>
+<body>
+    <div class="jumbotron">
+        <img src="images/logo.png" class="center-block img-responsive"/>
+    </div>
+    
+    <div class="container">
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Repository</h3>
+                    <p>
+                        <a class="btn btn-default" href="https://bitbucket.org/" target="_blank">BitBucket &raquo;</a>
+                        <a class="btn btn-default" href="https://github.com/mamori017" target="_blank">GitHub &raquo;</a>
+                    </p>
+                </div>
+
+                <div class="col-md-6">
+                    <h3>Schedule</h3>
+                    <p>
+                        <a class="btn btn-default" href="https://mamori017.backlog.jp/" target="_blank">Backlog &raquo;</a>
+                        <a class="btn btn-default" href="https://brabioproject.appspot.com/account/login/" target="_blank">Brabio! &raquo;</a>
+                    </p>
+                </div>
+
+                <div class="col-md-6">
+                    <h3>Social</h3>
+                    <p>
+                        <a class="btn btn-default" href="http://booklog.jp/users/mamori017" target="_blank">BookLog &raquo;</a>
+                        <a class="btn btn-default" href="http://mamori017.hatenablog.com/" target="_blank">Hatena Blog &raquo;</a>
+                        <a class="btn btn-default" href="https://twitter.com/mamori017" target="_blank">Twitter &raquo;</a>
+                        <a class="btn btn-default" href="https://mamori017.slack.com/" target="_blank">Slack &raquo;</a>
+                    </p>
+                </div>
+
+                <div class="col-md-6">
+                    <h3>XaaS</h3>
+                    <p>
+                        <a class="btn btn-default" href="https://console.aws.amazon.com/" target="_blank">AWS &raquo;</a>
+                        <a class="btn btn-default" href="https://portal.azure.com/" target="_blank">Azure &raquo;</a>
+                        <a class="btn btn-default" href="https://id.heroku.com/login" target="_blank">Heroku &raquo;</a>                   
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <h3>Timeline</h3>
+            <p>
+               <a class="twitter-timeline" href="https://twitter.com/mamori017">Tweets by mamori017</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </p>
+        </div>
+
+
+        <footer class="text-center">
+            <p>&copy; 2017 mamori017</p>
+        </footer>
+    </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
