16a3b22c4c711802c1ac0357a50ca31acd5c6063
mamori017 <mamori017@outlook.jp>
2017-07-12 23:15:25 +0900

    For now

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta content="mamori017" name="Keywords" />
-	<meta content="mamori017" name="Description" />
-	<meta content="mamori017" name="Author" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>mamori017.azurewebsites.net</title>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
-	<link href="css/style.css" rel="stylesheet"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="mamori017" name="Keywords" />
+    <meta content="mamori017" name="Description" />
+    <meta content="mamori017" name="Author" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>mamori017.azurewebsites.net</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
     <div class="jumbotron">
@@ -26,7 +28,8 @@
     ?>        
     <div class="container">
         <div class="col-md-12 text-right">
-            <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://mamori017.azurewebsites.net/" data-text="Test tweet" data-size="default" data-hashtags="mamori017dev">Tweet</a>
+            <iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fmamori017.azurewebsites.net%2F&layout=button&size=large&mobile_iframe=true&width=75&height=28&appId" width="75" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+            <a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-url="http://mamori017.azurewebsites.net/" data-text="Test tweet" data-size="default" data-hashtags="mamori017dev">Tweet</a>
         </div>
         <div class="col-md-8">
             <div class="row">
@@ -66,7 +69,7 @@
         <div class="col-md-4">
             <h4>Timeline</h4>
             <p>
-               <a class="twitter-timeline" href="https://twitter.com/mamori017">Tweets by mamori017</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+               <a data-height="300"class="twitter-timeline" href="https://twitter.com/mamori017">Tweets by mamori017</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
             </p>
         </div>
     </div>
